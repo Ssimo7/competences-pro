2a5d3dd253e4c975b59a63a2ea5e4245fa58480b
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2018-11-09 18:27:20 +0100

    Soumet une première version du modèle
    Ceci est un premier jet. Le fichier `stock.json` contient une description de la specifications :
    - le nombre de contenant,
    - la position et la taille de chaque contenant
    - la quantité contenue
    - le type de contenant
    
    L'exploitation du modèle n'est pas opérationnelle pour l'instant

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import '../styles/app.scss';
+import stock from '../model/stock.json';
 
 export default class TestClass {
 
@@ -12,3 +13,23 @@ export default class TestClass {
 
 }
 
+let test = new TestClass();
+
+function computeRelativePosition(distance, distanceTotale) {
+  return (distance * 100 / distanceTotale) + "%";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log(stock.contenants);
+
+  let zone1 = document.createElement("zone1");
+  zone1.style.position = "absolute";
+  let contenant = stock.contenants[0];
+  zone1.style.left = computeRelativePosition(contenant.posX, stock.width);
+  zone1.style.top = computeRelativePosition(contenant.posY, stock.height);
+  zone1.style.width = computeRelativePosition(contenant.width, stock.width);
+  zone1.style.height = computeRelativePosition(contenant.height, stock.height);
+  zone1.style.backgroundColor = "red";
+  let stockElement = document.getElementById("stock");
+  stockElement.appendChild(zone1);
+}, false);
