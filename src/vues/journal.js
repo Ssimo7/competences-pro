2f29f515ad5ef407f87ef4efe1422b3745605ead
Étienne Charignon <etienne.charignon@beta.gouv.fr>
2018-12-27 10:20:31 +0100

    affiche le bouton de remise à zero du journal

--- src/vues/journal.js
+++ src/vues/journal.js
@@ -1,6 +1,12 @@
 export class VueJournal {
   constructor (pointInsertion, journal) {
     this.journal = journal;
+
+    this.reset = document.createElement('button');
+    this.reset.id = 'resetjournal';
+    this.reset.textContent = 'effacer le journal';
+    pointInsertion.appendChild(this.reset);
+
     this.element = document.createElement('pre');
     this.element.id = 'journal';
     document.querySelector(pointInsertion).appendChild(this.element);
--- tests/vues/journal.js
+++ tests/vues/journal.js
@@ -4,6 +4,7 @@ import jsdom from 'jsdom-global';
 describe('vue journal', function () {
   let vue;
   let journal;
+  let pointInsertion;
 
   beforeEach(function () {
     jsdom('<div id="restitution"></div>');
@@ -12,11 +13,11 @@ describe('vue journal', function () {
         return [ 'ligne1', 'ligne2' ];
       }
     };
+    pointInsertion = document.getElementById('restitution');
     vue = new VueJournal('#restitution', journal);
   });
 
   it('initialise une zone de texte', function () {
-    let pointInsertion = document.getElementById('restitution');
     expect(pointInsertion.firstChild.id).to.equal('journal');
   });
 
@@ -24,5 +25,11 @@ describe('vue journal', function () {
     vue.affiche();
 
     expect(document.getElementById('journal').textContent).to.equal('"ligne1"\n"ligne2"');
+  });
+
+  it('affiche un bouton de remise à zero du journal', function () {
+    const boutonReset = pointInsertion.querySelector('#resetjournal');
+    expect(boutonReset).to.not.be(null);
+    expect(boutonReset.textContent).to.equal('effacer le journal');
   });
 });
